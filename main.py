63fcecb6824725ad6d1ec549fe7b1113e3da32d7
O123mk <mbredoil02@gmail.com>
2025-05-21 18:09:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -529,6 +529,11 @@ async def txt_handler(bot: Client, m: Message):
     raw_text4 = input4.text
     await input4.delete(True)
 
+    await editable.edit(f"**🔹Send the Video Thumb watermark Name**")
+    input6 = message = await bot.listen(editable.chat.id)
+    text44 = input6.text
+    await input6.delete(True)
+
     await editable.edit(f"**🔹Send the Video Thumb URL\n🔹Send /d for use default\n\n🔹You can direct upload thumb\n🔹Send **No** for use default**")
     input6 = message = await bot.listen(editable.chat.id)
     raw_text6 = input6.text
